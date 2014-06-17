2b3b0f07cf178d42cf991685c4c913899d0b0a9f
Ryan Hartman <ryandavidhartman@gmail.com>
2014-06-17 19:59:35 -0400

    Added some more integration unit tests

--- Auth404.IntegrationTests/UserRegistrationTests.cs
+++ Auth404.IntegrationTests/UserRegistrationTests.cs
@@ -18,15 +18,37 @@ namespace Auth404.IntegrationTests
         [TestFixtureSetUp]
         public void set_up()
         {
+           
             WebServiceHostUrl = ConfigurationManager.AppSettings["ServerUrl"];
             RestClient = new JsonServiceClient(WebServiceHostUrl);
 
             DataBaseHelper.Settup_Test_Database();
         }
 
+        [TestFixtureTearDown]
+        public void close_down()
+        {
+            Logout();
+        }
+
+
+
+        public void Logout()
+        {
+            // ReSharper disable RedundantTypeArgumentsOfMethod
+            //logout
+            RestClient.Post(new Authenticate {provider = "logout"});
+
+            var checkLoginStatus = RestClient.Post<AuthenticateResponse>(new Authenticate());
+            Assert.IsNotNull(checkLoginStatus);
+            Assert.IsNullOrEmpty(checkLoginStatus.UserName);
+            // ReSharper restore RedundantTypeArgumentsOfMethod
+        }
+
         [Test]
         public void can_register_a_new_user_and_log_them_on()
         {
+            // ReSharper disable RedundantTypeArgumentsOfMethod
             var createRequest = new UserRegistrationRequest
             {
                 Email = "user1@gmail.com",
@@ -41,11 +63,16 @@ namespace Auth404.IntegrationTests
             var checkLoginStatus = RestClient.Post<AuthenticateResponse>(new Authenticate());
             Assert.IsNotNull(checkLoginStatus);
             Assert.AreEqual(createRequest.Email, checkLoginStatus.UserName);
+
+            //logout
+            Logout();
+            // ReSharper restore RedundantTypeArgumentsOfMethod
         }
 
         [Test]
         public void test_login_logout_using_credential_auth()
         {
+            // ReSharper disable RedundantTypeArgumentsOfMethod
             var createRequest = new UserRegistrationRequest
             {
                 Email = "user2@gmail.com",
@@ -58,28 +85,26 @@ namespace Auth404.IntegrationTests
             Assert.IsTrue(createResponse.UserId.Length > 0);
 
             //logout - in case we had an open session
-            RestClient.Post(new Authenticate { provider = "logout" });
+            RestClient.Post(new Authenticate {provider = "logout"});
             var checkLoginStatus = RestClient.Post<AuthenticateResponse>(new Authenticate());
             Assert.IsNotNull(checkLoginStatus);
             Assert.IsNullOrEmpty(checkLoginStatus.UserName);
 
-            
+
             // login the user in
-            checkLoginStatus = RestClient.Post<AuthenticateResponse>(new Authenticate { provider = "credentials", UserName = "user2@gmail.com", Password = "user2"});
+            checkLoginStatus = RestClient.Post<AuthenticateResponse>(new Authenticate {provider = "credentials", UserName = "user2@gmail.com", Password = "user2"});
             Assert.IsNotNull(checkLoginStatus);
             Assert.AreEqual("user2@gmail.com", checkLoginStatus.UserName);
 
             //logout
-            RestClient.Post(new Authenticate { provider = "logout" });
-            checkLoginStatus = RestClient.Post<AuthenticateResponse>(new Authenticate());
-            Assert.IsNotNull(checkLoginStatus);
-            Assert.IsNullOrEmpty(checkLoginStatus.UserName);
-
+            Logout();
+            // ReSharper restore RedundantTypeArgumentsOfMethod
         }
 
         [Test]
         public void test_login_logout_using_basic_auth()
         {
+            // ReSharper disable RedundantTypeArgumentsOfMethod
             var createRequest = new UserRegistrationRequest
             {
                 Email = "user3@gmail.com",
@@ -92,7 +117,7 @@ namespace Auth404.IntegrationTests
             Assert.IsTrue(createResponse.UserId.Length > 0);
 
             //logout - in case we had an open session
-            RestClient.Post(new Authenticate { provider = "logout" });
+            RestClient.Post(new Authenticate {provider = "logout"});
             var checkLoginStatus = RestClient.Post<AuthenticateResponse>(new Authenticate());
             Assert.IsNotNull(checkLoginStatus);
             Assert.IsNullOrEmpty(checkLoginStatus.UserName);
@@ -109,11 +134,79 @@ namespace Auth404.IntegrationTests
             Assert.AreEqual("user3@gmail.com", checkLoginStatus.UserName);
 
             //logout
-            RestClient.Post(new Authenticate { provider = "logout" });
+            RestClient.Post(new Authenticate {provider = "logout"});
             checkLoginStatus = RestClient.Post<AuthenticateResponse>(new Authenticate());
             Assert.IsNotNull(checkLoginStatus);
             Assert.IsNullOrEmpty(checkLoginStatus.UserName);
+            // ReSharper restore RedundantTypeArgumentsOfMethod
+        }
 
+        [Test]
+        public void user_can_update_their_registration()
+        {
+            // ReSharper disable RedundantTypeArgumentsOfMethod
+            var createRequest = new UserRegistrationRequest
+            {
+                Email = "user4@gmail.com",
+                Password = "user4",
+                AutoLogin = true
+            };
+
+            var createResponse = RestClient.Post<UserRegistrationResponse>(createRequest);
+            Assert.IsNotNull(createResponse);
+            Assert.IsTrue(createResponse.UserId.Length > 0);
+
+            var checkLoginStatus = RestClient.Post<AuthenticateResponse>(new Authenticate());
+            Assert.IsNotNull(checkLoginStatus);
+            Assert.AreEqual(createRequest.Email, checkLoginStatus.UserName);
+
+            //update the password
+            var updateRequest = new UserRegistrationRequest {Password = "UPDATED"};
+            var updateResponse = RestClient.Put<UserRegistrationResponse>(updateRequest);
+            Assert.IsNotNull(updateResponse);
+            Assert.IsTrue(updateResponse.UserId.Length > 0);
+
+            //logout
+            Logout();
+
+            //logon with the new password
+            checkLoginStatus = RestClient.Post<AuthenticateResponse>(new Authenticate {provider = "credentials", UserName = "user4@gmail.com", Password = "UPDATED"});
+            Assert.IsNotNull(checkLoginStatus);
+            Assert.AreEqual("user4@gmail.com", checkLoginStatus.UserName);
+
+            //logout
+            Logout();
+            // ReSharper restore RedundantTypeArgumentsOfMethod-
+        }
+
+        [Test]
+        public void user_can_not_update_if_not_authenticated()
+        {
+            // ReSharper disable RedundantTypeArgumentsOfMethod
+            var createRequest = new UserRegistrationRequest
+            {
+                Email = "user5@gmail.com",
+                Password = "user5",
+                AutoLogin = true
+            };
+
+            var createResponse = RestClient.Post<UserRegistrationResponse>(createRequest);
+            Assert.IsNotNull(createResponse);
+            Assert.IsTrue(createResponse.UserId.Length > 0);
+
+
+            var checkLoginStatus = RestClient.Post<AuthenticateResponse>(new Authenticate());
+            Assert.IsNotNull(checkLoginStatus);
+            Assert.AreEqual(createRequest.Email, checkLoginStatus.UserName);
+
+            //logout
+            Logout();
+
+            // try and fail update the password
+            var updateRequest = new UserRegistrationRequest {Email = "user5@gmail.com", Password = "UPDATED"};
+            var error = Assert.Throws<WebServiceException>(() => RestClient.Put<UserRegistrationResponse>(updateRequest));
+            Assert.AreEqual("Unauthorized", error.Message);
+            // ReSharper restore RedundantTypeArgumentsOfMethod
         }
     }
 }
